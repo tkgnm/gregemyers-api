da394fb3f4b42cf306690bdcba0dabce74a6d9b0
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-05-13 21:44:35 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
